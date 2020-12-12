3aec12d1871ef2fb7888b6acc19691aad11f3926
Malik Piara <malikpiara@gmail.com>
2020-12-12 01:45:38 +0000

    added pages and source code from cs50

--- app/main.py
+++ app/main.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
-@app.route('/')
 
+@app.route("/")
 def index():
-    return "<h1>Welcome</h1>"
+    return render_template("index.html", title="Home")
 
+@app.route("/about")
+def about():
+    return render_template("about.html", title="About")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html", title="Contact")
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html", title="Projects")
